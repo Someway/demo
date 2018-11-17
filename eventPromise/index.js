1daca9f96d0be007f47cb2de9d69abdb3a688409
wanglin <wanglin@tongzhuogame.com>
2018-11-17 09:55:50 +0800

    change eventPromise
    one event counterpart one promise

--- eventPromise/index.js
+++ eventPromise/index.js
@@ -1,9 +1,25 @@
+const eventMap = new Map();
 function eventPromise(target, eventType) {
+  // not first
+  let resolves = eventMap.get(target);
+  if (Array.isArray(resolves)) {
+    return new Promise((res) => {
+      resolves.push(res);
+    });
+  }
+  // first add
+  resolves = [];
+  eventMap.set(target, resolves);
   return new Promise(function(res, rej) {
+    resolves.push(res);
     try {
       target.addEventListener(eventType, function f(e) {
+        res = resolves.shift();
         res(e);
-        target.removeEventListener(eventType, f);
+        if (resolves.length == 0) {
+          eventMap.delete(target);
+          target.removeEventListener(eventType, f);
+        }
       });
     } catch (error) {
       rej(error);
@@ -13,10 +29,10 @@ function eventPromise(target, eventType) {
 
 export const eventPromiseWithCallback = (target, eventType, fn) => {
   return eventPromise(target, eventType)
-    .then(e => {
+    .then((e) => {
       return fn.call(target, null, e);
     })
-    .catch(err => {
+    .catch((err) => {
       return fn.call(target, err);
     });
 };
